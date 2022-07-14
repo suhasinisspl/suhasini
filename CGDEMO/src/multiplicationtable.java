7909daa8c72f479e805e476bf105f678f0b28e8a
suhasini <suhasini@192.168.0.106>
2022-07-14 12:21:55 +0530

    multiplication

---
+++ CGDEMO/src/multiplicationtable.java
@@ -0,0 +1,14 @@
+public class multiplicationtable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      int num = 4;
+	        for(int i = 1; i <= 20; ++i)
+	        {
+	            System.out.printf("%d * %d = %d \n", num, i, num * i);
+	        }
+	    }
+
+	}
+
+
